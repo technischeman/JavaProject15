774f176d16b692e05f6e106836eb7a1672445739
Samsung <cengizdemir1991@web.de>
2021-03-13 23:30:26 +0100

    four operations

---
+++ src/fouroperations.java
@@ -0,0 +1,20 @@
+public class fouroperations {
+    private int sayi1;
+    private int sayi2;
+    public int sum(int sayi1, int sayi2){
+        int toplam=sayi1+sayi2;
+        return toplam;
+    }
+    public int sub(int sayi1,int sayi2){
+        int cikar=sayi1-sayi2;
+        return cikar;
+    }
+    public int multi(int sayi1,int sayi2){
+        int carp =sayi1*sayi2;
+        return carp;
+    }
+    public int sep(int sayi1,int sayi2){
+        int böl =sayi1/sayi2;
+        return böl;
+    }
+}
---
+++ src/main.java
@@ -0,0 +1,14 @@
+public class main {
+    public static void main(String[] args) {
+  fouroperations islem=new fouroperations();
+  int abc=islem.sum(11,12);
+        System.out.println(abc);
+        System.out.println(islem.sub(12,13));
+        System.out.println(islem.multi(12,13));
+        System.out.println(islem.sum(12,13));
+        System.out.println(islem.sep(12,13));
+
+
+
+    }
+}
